5802130a02be8298740ee8cdaaf007b9790b82b4
George <shegerageorgy@gmail.com>
2024-06-11 17:00:34 +0300

    Leave Management CRUD

--- LeaveManagementSystem.Web/Data/ApplicationDbContext.cs
+++ LeaveManagementSystem.Web/Data/ApplicationDbContext.cs
@@ -6,8 +6,7 @@ namespace LeaveManagementSystem.Web.Data
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+            : base(options) { }
+        public DbSet<LeaveType> LeaveTypes { get; set; }
     }
 }
---
+++ LeaveManagementSystem.Web/Data/LeaveType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagementSystem.Web.Data
+{
+    public class LeaveType
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(150)]
+        public string Name { get; set; } = "";
+        public int NumberOfDays { get; set; }
+    }
+}
